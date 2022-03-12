7dd671b20d452679f515fd33dbcb7ea14256bc3a
abhinav1912 <abhinavmathur1912@gmail.com>
2022-03-12 05:06:30 +0000

    refactor: injected utility helper into SHA256 class

--- SHA256.py
+++ SHA256.py
@@ -1,8 +1,9 @@
 from typing import List
 
 class SHA256:
-    def __init__():
-        pass
+    def __init__(self, helper: SHA256_utility):
+        self.hash_values = helper.get_hash_constants()
+        self.round_constants = helper.get_round_value_constants()
 
     def convert_string_to_binary(self, string: str = "") -> List[int] :
         pass
