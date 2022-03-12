3ff3ecfe90cefcb4a73d78fc1e3ff14fcc0465d9
abhinav1912 <abhinavmathur1912@gmail.com>
2022-03-12 04:58:30 +0000

    added class interface

---
+++ SHA256.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class SHA256:
+    def __init__():
+        pass
+
+    def convert_string_to_binary(self, string: str = "") -> List[int] :
+        pass
+
+    def create_message_schedule(self, message: list = []) -> List[str]:
+        pass
+
+    def compression(self, message: list = []) -> List[str]:
+        pass
+
+    def modify_hash_values(self, values = []) -> List[str]:
+        pass
+
+    def create_final_hash(self) -> str:
+        pass
+
+    @staticmethod
+    def generate_SHA256_hash(word: str) -> str:
+        pass
