b30928dca583ae28926bedde230e98c68e6bdd79
abhinav1912 <abhinavmathur1912@gmail.com>
2022-03-12 05:03:52 +0000

    wip: added utility class interface

---
+++ SHA256_utility.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class SHA256_utility:
+    def __init__(self):
+        pass
+
+    def get_hash_constants(self) -> List[str]:
+        pass
+
+    def get_round_value_constants(self) -> List[str]:
+        pass
